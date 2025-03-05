75cd569bda2ad73a43c6fc63449d419e6936ce43
Sped0n <hi@sped0n.com>
2025-03-05 17:14:02 +0800

    refactor: optimize memory handling and refactor code for efficiency
    - Optimize `memMove` function by simplifying the logic and improving pointer arithmetic.
    - Remove redundant `safeStrCopy` function and replace it with a more efficient version for fixed-size process name copying.
    - Clean up and refactor code for better readability and maintainability.
    
    Signed-off-by: Sped0n <hi@sped0n.com>

--- src/misc.zig
+++ src/misc.zig
@@ -1,34 +1,24 @@
 ///https://github.com/ziglang/zig/blob/52ba2c3a43a88a4db30cff47f2f3eff8c3d5be19/lib/std/special/c.zig#L115
 pub fn memMove(dest: [*]u8, src: [*]const u8, n: usize) [*]u8 {
-    if (@intFromPtr(dest) < @intFromPtr(src)) {
-        var index: usize = 0;
-        while (index != n) : (index += 1) {
-            dest[index] = src[index];
+    const src_addr = @intFromPtr(src);
+    const dest_addr = @intFromPtr(dest);
+
+    if (src_addr < dest_addr and (src_addr + n) > dest_addr) {
+        var i = n;
+        while (i > 0) : (i -= 1) {
+            dest[i] = src[i];
         }
     } else {
-        var index = n;
-        while (index != 0) {
-            index -= 1;
-            dest[index] = src[index];
+        var i: usize = 0;
+        while (i < n) : (i += 1) {
+            dest[i] = src[i];
         }
     }
-
-    return dest;
 }
 
-///Like strncpy but guaranteed to NUL-terminate.
-pub fn safeStrCopy(s: [*]u8, t: [*]const u8, n: usize) [*]u8 {
-    const os = s;
-
-    if (n <= 0) {
-        return os;
-    }
-
-    var i: usize = 0;
-    while (i < n - 1 and t[i] != 0) : (i += 1) {
-        s[i] = t[i];
-    }
-
-    s[i] = 0;
-    return os;
+///We only use this in process name copying, so its okay to use fixed size.
+pub fn safeStrCopy(dest: *[16]u8, src: []const u8) void {
+    const len = @min(src.len, dest.len - 1);
+    @memcpy(dest[0..len], src[0..len]);
+    dest[len + 1] = 0;
 }
