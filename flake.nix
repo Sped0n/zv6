cfe61fd1078bf81b1d2366307a9b7948cdf51053
Sped0n <hi@sped0n.com>
2025-05-04 21:24:12 +0800

    build: add Nix dev shell and Python 3.11 to tool configuration
    - Add a new flake.nix file defining a Nix dev shell with qemu, lldb, and zsh for aarch64-darwin
    - Add Python 3.11 to the mise.toml tool configuration
    
    Signed-off-by: Sped0n <hi@sped0n.com>

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "zv6";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    system = "aarch64-darwin";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+      pkgs.mkShell {
+        packages = with pkgs; [
+          qemu
+          lldb
+          zsh
+        ];
+        shellHook = ''
+          exec zsh
+        '';
+      };
+  };
+}
