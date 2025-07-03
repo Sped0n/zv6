708a8dff158352a510bdb94e5f68749c00297458
Sped0n <hi@sped0n.com>
2025-07-03 21:21:43 +0800

    chore: improve development environment with updated shell and config files
    Signed-off-by: Sped0n <hi@sped0n.com>

--- flake.nix
+++ flake.nix
@@ -1,8 +1,5 @@
 {
   description = "zv6";
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-  };
   outputs = {
     self,
     nixpkgs,
@@ -14,15 +11,36 @@
       pkgs = import nixpkgs {
         inherit system;
       };
+      venvDir = "./.venv";
     in
-      pkgs.mkShell {
+      pkgs.mkShellNoCC {
+        name = "zv6";
         packages = with pkgs; [
+          zig
           qemu
           lldb
-          zsh
+          (python312.withPackages (p:
+            with p; [
+              virtualenv
+            ]))
         ];
-        shellHook = ''
-          exec zsh
+        shellHook = with pkgs; ''
+          SOURCE_DATE_EPOCH=$(date +%s)
+
+          if [ -d "${venvDir}" ]; then
+            echo "Skipping venv creation, '${venvDir}' already exists"
+          else
+            echo "Creating new venv environment in path: '${venvDir}'"
+            # Note that the module venv was only introduced in python 3, so for 2.7
+            # this needs to be replaced with a call to virtualenv
+            ${python312Packages.python.interpreter} -m venv "${venvDir}"
+          fi
+
+          # Under some circumstances it might be necessary to add your virtual
+          # environment to PYTHONPATH, which you can do here too;
+          # PYTHONPATH=$PWD/${venvDir}/${python312Packages.python.sitePackages}/:$PYTHONPATH
+
+          source "${venvDir}/bin/activate"
         '';
       };
   };
